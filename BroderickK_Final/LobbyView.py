5b6cfdde4366a683b612fa0854cb5e4f76b99a01
randomizerkatt <ABroderick@student.neumont.edu>
2021-03-08 12:57:47 -0700

    Adding characters

--- BroderickK_Final/LobbyView.py
+++ BroderickK_Final/LobbyView.py
@@ -1,35 +1,39 @@
 from tkinter import *
 
 lobby_canvas = ''
-player_character = ''
+player_character = []
 
 def left(event):
     global lobby_canvas
     global player_character
     x = -15 # Left / right
     y = 0 # Up / down
-    lobby_canvas.move(player_character, x, y)
+    for piece in player_character:
+        lobby_canvas.move(piece, x, y)
 
 def right(event):
     global lobby_canvas
     global player_character
     x = 15 # Left / right
     y = 0 # Up / down
-    lobby_canvas.move(player_character, x, y)
+    for piece in player_character:
+        lobby_canvas.move(piece, x, y)
 
 def up(event):
     global lobby_canvas
     global player_character
     x = 0 # Left / right
     y = -15 # Up / down
-    lobby_canvas.move(player_character, x, y)
+    for piece in player_character:
+        lobby_canvas.move(piece, x, y)
 
 def down(event):
     global lobby_canvas
     global player_character
     x = 0 # Left / right
     y = 15 # Up / down
-    lobby_canvas.move(player_character, x, y)
+    for piece in player_character:
+        lobby_canvas.move(piece, x, y)
 
 def pressing(event):
     if event.char == "a":
@@ -42,7 +46,7 @@ def pressing(event):
         up('')
 
 def location_tracker(event):
-    
+    pass
 
 def create_frame(base_root):
     global lobby_canvas
@@ -65,29 +69,45 @@ def create_frame(base_root):
     canvas_background.background = image_background
     bg = canvas_background.create_image(0, 0, anchor=NW, image=image_background)
 
+    x = 45
+    # Customers
+    mr_blueberry = canvas_background.create_oval(x, y-50, x+100, y+300, fill="blue")
+
+    x = 525
+    miss_strawberry = canvas_background.create_oval(x, y-50, x+100, y+300, fill="pink")
+
+    x = 775
+    miss_anxious = canvas_background.create_oval(x, y-50, x+100, y+300, fill="yellow")
+
     # Character
     # character_location = Frame(f, width=450, height=450)
     # character_canvas = Canvas(character_location, width=300, height=300)
 
-    # character_canvas.create_oval(100,50,150,100, fill='gray90')
+    y = 150
+    head = canvas_background.create_oval(100, y,150, y +100, fill='gray90')
+    player_character.append(head)
 
-    # x = 125
-    # y = 175
-    # stick = character_canvas.create_line(x, y-75, x, y)
+    x = 125
+    y += 175
+    stick = canvas_background.create_line(x, y-75, x, y)
+    player_character.append(stick)
 
-    # diff_x = 25
-    # stick_leg1 = character_canvas.create_line(x, y, x-diff_x, y+50)
-    # stick_leg2 = character_canvas.create_line(x, y, x+diff_x, y+50)
+    diff_x = 25
+    stick_leg1 = canvas_background.create_line(x, y, x-diff_x, y+50)
+    player_character.append(stick_leg1)
+    stick_leg2 = canvas_background.create_line(x, y, x+diff_x, y+50)
+    player_character.append(stick_leg2)
 
-    # y=145
-    # stick_arm1 = character_canvas.create_line(x, y, x-30, y-20)
-    # stick_leg2 = character_canvas.create_line(x, y, x+30, y-10)
+    y -= 30
+    stick_arm1 = canvas_background.create_line(x, y, x-30, y-20)
+    player_character.append(stick_arm1)
+    stick_arm2 = canvas_background.create_line(x, y, x+30, y-10)
+    player_character.append(stick_arm2)
 
     # character_canvas.pack()
     # character_location.pack()
 
 
-    my_circle = canvas_background.create_oval(x, y-50, x+75, y+350, fill="blue")
     # create_rectangle
 
     base_root.bind("<Key>", pressing)
@@ -96,10 +116,9 @@ def create_frame(base_root):
     base_root.bind("<Right>", right)
     base_root.bind("<Up>", up)
     base_root.bind("<Down>", down)
-    base_root.bind("<Space>", location_tracking)
+    base_root.bind("<space>", location_tracker)
 
     lobby_canvas = canvas_background
-    player_character = my_circle
 
     return f
 
